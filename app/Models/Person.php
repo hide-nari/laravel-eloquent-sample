30d6baf2d9c43a567ef1a057694aa7dbe25b57c3
watanabeHi <hide3@mild.ocn.ne.jp>
2026-01-11 05:59:56 +0900

    Add person model attribute logic

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -15,4 +16,12 @@ class Person extends Model
             'name',
             'age',
         ];
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value, array $attributes) => 'Mr.' . $value,
+            set: fn($value) => $value,
+        );
+    }
 }
