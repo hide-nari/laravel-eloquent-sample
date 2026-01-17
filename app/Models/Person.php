ea89656cf79d914258a568787be0a7ee147de20a
watanabeHi <hide3@mild.ocn.ne.jp>
2026-01-18 05:00:57 +0900

    Code formatter

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -21,7 +21,7 @@ class Person extends Model
     protected function name(): Attribute
     {
         return Attribute::make(
-            get: fn ($value, array $attributes) => 'Mr.'.$value,
+            get: fn($value, array $attributes) => 'Mr.'.$value,
             set: function ($value) {
                 if (mb_strlen($value) < 2 || mb_strlen($value) > 30) {
                     throw new InvalidArgumentException('name length invalid');
@@ -35,7 +35,7 @@ class Person extends Model
     protected function age(): Attribute
     {
         return Attribute::make(
-            set: fn ($value) => $value >= 15 ? $value
+            set: fn($value) => $value >= 15 ? $value
                 : throw new InvalidArgumentException('under fifteen'),
         );
     }
